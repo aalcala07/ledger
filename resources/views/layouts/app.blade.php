920c3e46d670122a2b871296ef494508a007ba0c
Anthony Alcala <anthony@aalcala.com>
2021-07-16 15:46:30 -0500

    Update browser title.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ config('app.env') === 'local' ? "Ledger (local)" : "Ledger" }}</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="url" content="{{ config('app.url') }}">
